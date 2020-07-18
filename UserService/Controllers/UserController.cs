e094ef03307efb6773706b7fa54fdaf50be96f00
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-07-18 21:33:23 +0300

    Add project files.

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Database.Context;
+using UserService.Database.Entities;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserContext _db;
+        public UserController()
+        {
+            _db = new UserContext();
+        }
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _db.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            return _db.Users.Find(id);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            try
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,user);
+            }
+            catch (Exception ex) 
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+           
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserService/Database/Context/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=UserMicro; Trusted_Connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User { UserId = 1, Name = "Azra", Address= "Karaşar" });
+            modelBuilder.Entity<User>().HasData(new User { UserId = 2, Name = "Hazal", Address = "Ankara" });
+            modelBuilder.Entity<User>().HasData(new User { UserId = 3, Name = "Parla", Address = "İstanbul" });
+        }
+    }
+}
